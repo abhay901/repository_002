fb92bc2774f1f12452125f7fdbd1419d3c2107b1
abhay901 <abhay4cs@gmail.com>
2022-07-25 12:59:26 +0530

    hello

---
+++ src/test/java/org/practise/Test007.java
@@ -0,0 +1,18 @@
+package org.practise;
+
+import org.testng.annotations.Test;
+
+public class Test007 
+{	@Test
+	public void test()
+	{
+		System.out.println("Test is Passed");
+		System.out.println("Test is Passed");
+		System.out.println("Test is Passed");
+		System.out.println("Test is Passed");
+		System.out.println("Test is Passed");
+		System.out.println("Test is Passed");
+		System.out.println("Test is Passed");
+	}
+
+}
